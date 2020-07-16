8298d6638031c8127b9df45d48b5fc6634bc11d3
DaiQiangReal <daiqianghaha@foxmail.com>
2020-07-17 01:14:04 +0800

    配置 Vue-Router Vuex

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MainComponent',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
+import store from './store'
+import App from './App.vue'
+import MainComponent from "./components/MainComponent.vue"
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(Vuex)
+const router=[
+    {path:'/',component:MainComponent},
+]
+
 new Vue({
+    router,
+    store,
   render: h => h(App),
 }).$mount('#app')
